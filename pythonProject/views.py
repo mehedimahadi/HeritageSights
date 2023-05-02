b4df4643efcaf3c1a43bee5d12cfb9246a0ba138
AnikaTahsinRaka <20101029@uap-bd.edu>
2023-05-03 02:11:47 +0600

    Raka's contribution

---
+++ pythonProject/views.py
@@ -0,0 +1,28 @@
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def make_plan(request):
+    return render(request, 'make_plan.html')
+
+def profile_page(request):
+    return render(request, 'profile_page.html')
+
+def reset_with_mail(request):
+    return render(request, 'reset_with_mail.html')
+
+
+def new_pass(request):
+    return render(request, 'new_pass.html')
+
+def password_success(request):
+    return render(request, "home.html")
+
+def password_change_done(request):
+    return redirect('/')
