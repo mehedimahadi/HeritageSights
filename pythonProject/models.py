3a5930e81ad45cc6d68655f50a04efe46550b62b
Jannatul-Ferdaus87 <jannatulf2838@gmail.com>
2023-05-06 12:43:25 -0700

    Jannat's contribution

--- pythonProject/models.py
+++ pythonProject/models.py
@@ -1,3 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+# from PIL import Image
+
+
+class SignUp(models.Model):
+    User_id = models.AutoField
+    FullName = models.CharField(max_length=100)
+    UserName = models.CharField(max_length=100)
+    Email = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+
+
+class Signin(models.Model):
+    User_id = models.AutoField
+    UserName = models.CharField(max_length=100)
+    Email = models.CharField(max_length=100)
+
+
+
+
+
 class Contact(models.Model):
     User_id = models.AutoField
     UserName = models.CharField(max_length=50)
