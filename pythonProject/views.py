660fe234dd61ff13c3a396198193f99f249fc310
Jannatul-Ferdaus87 <jannatulf2838@gmail.com>
2023-05-05 19:15:10 -0700

    Jannat's contribution

--- pythonProject/views.py
+++ pythonProject/views.py
@@ -11,8 +11,20 @@ def contact(request):
 def make_plan(request):
     return render(request, 'make_plan.html')
 
+
+def home(request):
+    return render(request, 'home.html')
+
 def profile_page(request):
     return render(request, 'profile_page.html')
+
+def sign_in(request):
+    return render(request, 'sign_in.html')
+
+
+def sign_up(request):
+    return render(request, 'sign_up.html')
+
 
 def reset_with_mail(request):
     return render(request, 'reset_with_mail.html')
